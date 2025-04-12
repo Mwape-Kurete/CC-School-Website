5c3650d2c9f855aba15d8cc9aa343bd02786a65a
mwape-k <82158230+mwape-k@users.noreply.github.com>
2025-04-12 20:20:45 +0200

    CORE(feat): creating pages and links

--- CodeCloudSchool-Site/CCSchool-VueSite/src/router/index.ts
+++ CodeCloudSchool-Site/CCSchool-VueSite/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import CourseView from '../views/CourseView.vue'
+import ContactView from '../views/ContactView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +10,22 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: () => import('@/views/HomeView.vue')
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('@/views/AboutView.vue')
+    },
+    {
+      path: '/courses',
+      name: 'courses',
+      component: () => import('@/views/CourseView.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('@/views/ContactView.vue')
     },
   ],
 })
