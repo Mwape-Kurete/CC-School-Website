468646c93ce98471ebd903bfa9229c5f69f7baee
mwape-k <82158230+mwape-k@users.noreply.github.com>
2025-04-13 23:40:45 +0200

    Core: files recreate

---
+++ CodeCloudSchool-Site/CCSchool-VueSite/src/components/ContactForm.vue
@@ -0,0 +1,12 @@
+<script></script>
+
+<template>
+    <main class="contact-form">
+        <div class="grid">
+            <div class="grid col-span-6"></div>
+            <div class="grid col-span-6"></div>
+        </div>
+    </main>
+</template>
+
+<style></style>
--- CodeCloudSchool-Site/CCSchool-VueSite/src/views/ContactView.vue
+++ CodeCloudSchool-Site/CCSchool-VueSite/src/views/ContactView.vue
@@ -1,11 +1,23 @@
+<script>
+import ContactForm from '@/components/ContactForm.vue'; 
+</script>
+
 <template>
-    <div class="contact">
-        <h1 class="text-3xl font-bold mb-4">Contact Page</h1>
-        <p class="text-lg">This is where users can conctat the services providers.</p>
+  <main>
+    <div class="contact grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-4">
+        <div class="info-sect col-span-12">
+          <h1>We’d Love to Hear from You</h1>
+          <p>Have a question about our programs, enrollment, or certifications? Need help choosing the right course for your goals? Our team is here to help every step of the way.</p>
+          <p><i>Reach out to us and we’ll get back to you within 24–48 hours.</i></p>
+        </div>
+        <div class="contact-info-sect col-span-6"></div>
+        <div class="form-sect col-span-6"></div>
+        <div class="faq-sect col-span-12"></div>
     </div>
+  </main>
   </template>
 
-<style>
+<style scoped>
 @media (min-width: 1024px) {
   .contact {
     min-height: 25vh;        /* Makes section at least full viewport height */
@@ -14,6 +26,9 @@
     padding-top: 5rem;        /* Adds 80px space below fixed navbar (adjust as needed) */
     box-sizing: border-box;   /* Ensures padding is included in height calculation */
     padding-left: 3rem; 
+  }
+  .h1 {
+    color: black;
   }
 }
 </style>
